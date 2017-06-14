e5ea456749011f7bc9060c5355ca9645fe1b685d
Albert Lee <grepinsight@gmail.com>
2017-06-14 04:11:55 -0400

    Useful stuff

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,14 @@ if [[ -f $HOME/.bash_profile ]]; then
     echo "$HOME/.bash_profile already exists!"
 else
     echo "source $HOME/.dotfiles/bash/bashrc_init" >> $HOME/.bash_profile
+    echo "Making $HOME/.dotfiles/bash/local/"
+    mkdir -p $HOME/.dotfiles/bash/local/
+    echo "Creating $HOME/.dotfiles/bash/local/bash_aliases_local"
+    echo "Creating $HOME/.dotfiles/bash/local/bash_paths_local"
+    echo "Creating $HOME/.dotfiles/bash/local/bash_settings_local"
+    touch $HOME/.dotfiles/bash/local/bash_aliases_local
+    touch $HOME/.dotfiles/bash/local/bash_paths_local
+    touch $HOME/.dotfiles/bash/local/bash_settings_local
 fi
 
 
@@ -22,6 +30,19 @@ else
 
     echo " * Installing vim plugins"
     bash vim/vim_module_setup.sh
+fi
+
+### Tools
+
+echo "--- Downloading useful tools ---"
+if which git 2>/dev/null >&2 ; then 
+    if [[ ! -e $HOME/bin/bashmarks ]]; then
+        echo "installing bashmarks"
+        mkdir -p $HOME/bin
+        cd $HOME/bin && git clone https://github.com/huyng/bashmarks.git && cd - 
+    fi
+else
+    echo "git not found! install git and rerun this script again"
 fi
 
 #touch tmux/tmux.conf.local
