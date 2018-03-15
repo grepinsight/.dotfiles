7c7ac3fc4c7c17832cb428ba82449c7054872fae
Albert Lee <alee@counsyl.com>
2018-03-14 23:27:00 -0700

    Install VimPlug

--- vim/vim_module_setup.sh
+++ vim/vim_module_setup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 # set up essential vim modules
 
+
+# Install Pathogen(for backward compatibility)
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# Install VimPlug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 if cd $HOME/.vim/bundle; then
     # Essentials
