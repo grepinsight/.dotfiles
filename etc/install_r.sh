9eed89cdc5782e1c01843d3b0efe0cecce4bea50
Albert Lee <grepinsight@gmail.com>
2019-05-12 09:47:12 +0000

    update

---
+++ etc/install_r.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -eux
+
+sudo echo "deb http://cran.rstudio.com/bin/linux/ubuntu xenial/" | sudo tee -a /etc/apt/sources.list
+gpg --keyserver keyserver.ubuntu.com --recv-key E084DAB9
+gpg -a --export E084DAB9 | sudo apt-key add -
+
+
+sudo apt-get update
+sudo apt-get install r-base r-base-dev
