937bb839bf08daf5f7bee077a3f17e86538220eb
Albert Lee <grepinsight@gmail.com>
2019-10-21 13:37:51 -0700

    update

--- etc/install_core.ubuntu.sh
+++ etc/install_core.ubuntu.sh
@@ -3,10 +3,10 @@
 
 if $(which apt-get); then
 
-  sudo apt-get install libssl-dev           # Install Secure Sockets Layer toolkit
-  sudo apt install make                     # install make 
-  sudo apt-get install libcurl4-openssl-dev # Install curl 
-  sudo apt-get install libxml2-dev          # Instal xml
-  
+  sudo apt-get --yes install libssl-dev           # Install Secure Sockets Layer toolkit
+  sudo apt-get --yes install make                     # install make
+  sudo apt-get --yes install libcurl4-openssl-dev # Install curl
+  sudo apt-get --yes install libxml2-dev          # Instal xml
+
   sudo apt install silversearcher-ag         # Install ag
 fi
