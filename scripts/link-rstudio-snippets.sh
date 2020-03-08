e8d240f3f3b4bf2c8c437fca54305fc257da9837
Albert Lee <grepinsight@gmail.com>
2020-03-08 01:46:32 -0800

    update script

--- scripts/link-rstudio-snippets.sh
+++ scripts/link-rstudio-snippets.sh
@@ -5,5 +5,5 @@ set -x
 for snippet in rstudio/snippets/*; do
     snippet_name="$(basename $snippet)"
     ln -sf $HOME/.dotfiles/rstudio/snippets/"$snippet_name"\
-        $HOME/.R/snippets/"$snippet_name"
+        $HOME/.config/rstudio/snippets/"$snippet_name"
 done
