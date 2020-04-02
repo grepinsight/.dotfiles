3d9fcda4d867a15d4dbd35d049d605ac2f768848
Albert Lee <grepinsight@gmail.com>
2020-04-01 21:08:41 -0700

    test

--- scripts/link-rstudio-snippets-and-bindings.sh
+++ scripts/link-rstudio-snippets-and-bindings.sh
@@ -7,6 +7,7 @@ for item in snippets keybindings; do
     for snippet in rstudio/$item/*; do
         item_name="$(basename $snippet)"
         mkdir -p $HOME/.config/rstudio/$item/
+        touch $HOME/.config/rstudio/$item/"$item_name"
         ln -sf $HOME/.dotfiles/rstudio/$item/"$item_name" \
             $HOME/.config/rstudio/$item/"$item_name"
     done
