6285661b77f155a4f62f77328b619852bba72079
Albert Lee <grepinsight@gmail.com>
2020-04-01 21:00:57 -0700

    fix

--- scripts/link-rstudio-snippets-and-bindings.sh
+++ scripts/link-rstudio-snippets-and-bindings.sh
@@ -6,9 +6,8 @@ mkdir -p $HOME/.config/rstudio
 for item in snippets keybindings; do
     for snippet in rstudio/$item/*; do
         item_name="$(basename $snippet)"
-        if [[ -e $HOME/.dotfiles/rstudio/$item/"$item_name" ]];then
-            ln -sf $HOME/.dotfiles/rstudio/$item/"$item_name" \
-                $HOME/.config/rstudio/$item/"$item_name"
-        fi
+        mkdir -p $HOME/.config/rstudio/$item/
+        ln -sf $HOME/.dotfiles/rstudio/$item/"$item_name" \
+            $HOME/.config/rstudio/$item/"$item_name"
     done
 done
