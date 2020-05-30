259c4fdcc9f518fd172f7568750d680a19004c12
Albert Lee <grepinsight@gmail.com>
2020-05-30 17:05:10 +0900

    nvim: add install.sh

---
+++ nvim/install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -x
+
+IS_MAC=$(uname -a | grep Darwin)
+
+
+if $IS_MAC; then
+    echo "is mac"
+fi
+
+IS_UBUNTU=$(uname -a | grep -i Ubuntu)
+
+if $IS_UBUNTU; then
+    mkdir -p $HOME/src
+    cd $HOME/src && curl -O -L https://github.com/neovim/neovim/releases/download/v0.4.3/nvim-linux64.tar.gz
+    tar xvzf nvim-linux64.tar.gz
+fi
+
