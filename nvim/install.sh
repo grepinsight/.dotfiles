528ab23d5008e0cc35e95ada80586833b95e69de
Albert Lee <grepinsight@gmail.com>
2020-06-01 06:50:41 +0900

    nvim: installation, refactoring name and version

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+NEOVIM_DIST="nvim-linux64"
+NEOVIM_VERSION="v0.4.3"
 set -x
 
 IS_MAC=$(uname -a | grep Darwin)
@@ -13,11 +15,13 @@ IS_UBUNTU=$(uname -a | grep -i Ubuntu)
 
 if [[ -n $IS_UBUNTU ]]; then
     mkdir -p $HOME/src
-    cd $HOME/src && curl -O -L https://github.com/neovim/neovim/releases/download/v0.4.3/nvim-linux64.tar.gz
-    tar xvzf nvim-linux64.tar.gz
+    cd $HOME/src && curl -O -L https://github.com/neovim/neovim/releases/download/$NEOVIM_VERSION/$NEOVIM_DIST.tar.gz
+    tar xvzf $NEOVIM_DIST.tar.gz
 
-    sudo cp -r bin/* /usr/bin/
-    sudo cp -r share/* /usr/share/
+    if cd $HOME/src/$NEOVIM_DIST; then
+        sudo cp -r bin/* /usr/bin/
+        sudo cp -r share/* /usr/share/
+    fi
 
 fi
 
