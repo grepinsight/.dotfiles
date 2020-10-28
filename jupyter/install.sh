f47b4931d9377351c201034b0723ea56ec6d9a12
Albert Lee <grepinsight@gmail.com>
2020-10-28 10:41:12 -0700

    jupyter: paramterize jupyter dir

--- jupyter/install.sh
+++ jupyter/install.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+set -x
 
 echo "Install vim_binding"
 
+JUPYTER_DIR="$(jupyter --data-dir)"
 if [[ ! -d $(jupyter --data-dir)/nbextensions/vim_binding ]]; then
     # Create required directory in case (optional)
     mkdir -p $(jupyter --data-dir)/nbextensions
@@ -12,3 +14,7 @@ if [[ ! -d $(jupyter --data-dir)/nbextensions/vim_binding ]]; then
     # Activate the extension
     jupyter nbextension enable vim_binding/vim_binding
 fi
+
+
+mkdir -p ${JUPYTER_DIR}/nbconfig
+ln -sf $HOME/.dotfiles/jupyter/notebook.json  ${JUPYTER_DIR}/nbconfig/notebook.json
