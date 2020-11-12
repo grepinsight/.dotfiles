5d986b1721fef00cbbe95bf131abec707feea63a
Ubuntu <ubuntu@ip-10-129-1-87.us-west-2.compute.internal>
2020-11-12 19:32:03 +0000

    rust installer

--- rust/install.sh
+++ rust/install.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# check if rust exists
+command -v cargo || \
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# check if rust-analyzer exists
+IS_UBUNTU=$(uname -a | egrep -i "Ubuntu|Linux")
+if [[ -n $IS_UBUNTU ]]; then
+    mkdir -p $HOME/bin
+    command -v rust-analyzer || \
+        curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-linux -o ~/bin/rust-analyzer
+    chmod 750 $HOME/bin/rust-analyzer
+fi
+
