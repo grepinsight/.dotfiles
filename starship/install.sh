516af83831b590c675baf3bbb7b9b027f9df9561
Albert Lee <grepinsight@gmail.com>
2021-03-05 16:58:36 -0800

    starship installer update

--- starship/install.sh
+++ starship/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 
-command -v starship || curl -fsSL https://starship.rs/install.sh | bash
+command -v starship || curl -o starship_installer.sh -fsSL https://starship.rs/install.sh && bash starship_installer.sh --yes && rm starship_installer.sh
