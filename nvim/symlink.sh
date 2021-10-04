a4c677048c218d286a9a0c5ea6dde7276273efa1
Albert Lee <grepinsight@gmail.com>
2021-10-04 01:11:46 -0700

    vim: move ftplugin and mysnippets

--- nvim/symlink.sh
+++ nvim/symlink.sh
@@ -7,3 +7,5 @@ set -x
 ln -sf $HOME/.dotfiles/nvim/after $HOME/.config/nvim/after
 ln -sf $HOME/.dotfiles/nvim/init.lua $HOME/.config/nvim/init.lua
 ln -sf $HOME/.dotfiles/nvim/lua $HOME/.config/nvim/lua
+ln -sf $HOME/.dotfiles/nvim/ftplugin $HOME/.config/nvim/ftplugin
+ln -sf $HOME/.dotfiles/nvim/mysnippets $HOME/.config/nvim/mysnippets
