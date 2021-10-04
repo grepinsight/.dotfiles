94a691ba9abe5d1fe47c78e1d109c09fac5d43fb
Albert Lee <grepinsight@gmail.com>
2021-10-04 02:14:04 -0700

    settled

--- nvim/after/plugin/nvim-cmp.lua
+++ nvim/after/plugin/nvim-cmp.lua
@@ -23,13 +23,35 @@ cmp.setup {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
-    ['<Tab>'] = cmp.mapping(function(fallback)
-        --if vim.fn.pumvisible() == 1 then
-        --    cmp.mapping.select_next_item()
-        --else
-            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", true)
-        --end
-    end, {"i", "s"}),
+    -- ['<Tab>'] = cmp.mapping(function(fallback)
+    --     --if vim.fn.pumvisible() == 1 then
+    --     --    cmp.mapping.select_next_item()
+    --     if vim.fn.pumvisible() == 1 then
+    --         vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-n>', true, true, true), 'n')
+    --     else
+    --         vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", true)
+    --     end
+    -- end, {"i", "s"}),
+    -- ['<S-Tab>'] = function(fallback)
+    --   if vim.fn.pumvisible() == 1 then
+    --     vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-p>', true, true, true), 'n')
+    --   -- elseif luasnip.jumpable(-1) then
+    --   --   vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
+    --   else
+    --     vim.cmd(':<')
+    --   end
+    -- end,
+    -- ['<Tab>'] = function(core, fallback)
+    --   if vim.fn.pumvisible() == 1 then
+    --     vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-j>', true, true, true), 'n')
+    --   -- elseif luasnip.expand_or_jumpable() then
+    --   --   vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
+    --   -- elseif not check_back_space() then
+    --   --   cmp.mapping.complete()(core, fallback)
+    --   else
+    --     vim.cmd(':>')
+    --   end
+    -- end,
   },
   formatting = {
     format = function(entry, vim_item)
