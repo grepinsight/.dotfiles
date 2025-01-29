f3d3278d8fe64f2441bca8c05fb872a8c6040d3e
Albert Lee <grepinsight@gmail.com>
2025-01-29 06:50:55 -0800

    feat(themes): add multiple new git delta themes
    feat(jupyter): update shortcuts for cell output visibility in JupyterLab
    
    chore(neovim): modify keymap for find_files in telescope

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,5 +3,6 @@ local utils = require("telescope.utils")
 
 -- nnoremap <silent> <C-p> <cmd>lua require('telescope.builtin').find_files()<cr>
 vim.keymap.set("n", "<C-p>", function()
-  builtin.find_files({ cwd = utils.buffer_dir() })
+  --builtin.find_files({ cwd = utils.buffer_dir() })
+  builtin.find_files()
 end, {})
